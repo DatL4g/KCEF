72fa02f5a0a4f5337ef0057f0bc96c3351ea6986
DatLag <jeff.retz08@gmail.com>
2023-10-09 20:13:41 +0200

    fix not displaying on mac

--- jcef/build.gradle.kts
+++ jcef/build.gradle.kts
@@ -6,4 +6,5 @@ plugins {
 dependencies {
     implementation("org.jogamp.gluegen:gluegen-rt:2.5.0")
     implementation("org.jogamp.jogl:jogl-all:2.5.0")
+    implementation("net.java.dev.jna:jna-platform:5.13.0")
 }
